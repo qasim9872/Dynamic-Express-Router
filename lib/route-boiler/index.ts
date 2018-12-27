6ee49dd81f2fef602e242bf2603f0aae84aacdd8
Muhammad Qasim <Qasim9872@gmail.com>
2018-12-27 23:30:15 +0000

    updated file loader to use map and promise.all

--- lib/route-boiler/index.ts
+++ lib/route-boiler/index.ts
@@ -1,11 +1,11 @@
 import Router from "express-promise-router"
-import { join } from "path"
 import * as fse from "fs-extra"
+import { join } from "path"
 
 import config from "./config/default.config"
-import { getPath, normalize } from "./utils/helpers"
-import { createDebugger } from "./utils/debug"
 import { IRoute, requestTypes } from "./IRoute.interface"
+import { createDebugger } from "./utils/debug"
+import { getPath, normalize } from "./utils/helpers"
 
 const debug = createDebugger(__filename)
 
@@ -15,7 +15,7 @@ async function loadRouteFile(name: string, path: string): Promise<IRoute> {
   return {
     name: normalize(name),
     requestType: requestTypes.GET,
-    handler: routeFile.default,
+    handler: routeFile.default
   }
 }
 
@@ -24,27 +24,29 @@ async function loadRoutes(path: string, prefix = ``) {
 
   const files = await fse.readdir(path)
 
-  files.forEach(async (fileName, index) => {
-    const filePath = join(path, fileName)
-    const stats = await fse.lstat(filePath)
+  await Promise.all(
+    files.map(async (fileName) => {
+      const filePath = join(path, fileName)
+      const stats = await fse.lstat(filePath)
 
-    if (stats.isDirectory()) {
-      const subRoute = normalize(fileName)
-      const subRouter = await loadRoutes(filePath, `${prefix}${subRoute}`)
-      router.use(subRoute, subRouter)
-    } else {
-      const route = await loadRouteFile(fileName, filePath)
-      router.route(route.name)[route.requestType](route.handler)
-      debug(`${route.requestType}: ${prefix}${route.name}`)
-    }
-  })
+      if (stats.isDirectory()) {
+        const subRoute = normalize(fileName)
+        const subRouter = await loadRoutes(filePath, `${prefix}${subRoute}`)
+        router.use(subRoute, subRouter)
+      } else {
+        const route = await loadRouteFile(fileName, filePath)
+        router.route(route.name)[route.requestType](route.handler)
+        debug(`${route.requestType}: ${prefix}${route.name}`)
+      }
+    })
+  )
 
   return router
 }
 
 export default async function initialize(
   relativePath = config.DEFAULT_RELATIVE_PATH,
-  folderName = config.DEFAULT_FOLDER_NAME,
+  folderName = config.DEFAULT_FOLDER_NAME
 ) {
   const fullPath = getPath(relativePath, folderName)
   debug(`path to api folder: ${fullPath}`)
